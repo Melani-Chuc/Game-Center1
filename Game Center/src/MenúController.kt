107939f43cfe4c25c71b783be80791b9c6973a55
HCTORKEB <162727987+HCTORKEB@users.noreply.github.com>
2024-03-27 18:14:29 -0500

    Update MenúController.kt

--- Game Center/src/MenúController.kt
+++ Game Center/src/MenúController.kt
@@ -1,2 +1,39 @@
 class MenúController {
+  val ControllerUsuario = ControllerUsuario()
+  val ControllerEquipo = ControllerEquipo()
+
+    fun paginadeinicio() {
+        println("BIENVENIDO...")
+        println("( 1.)CREAR CUENTA")
+        println("( 2.)INICIAR SESION")
+        val paginicio = readln().toInt()
+        when (paginicio) {
+            1 -> {
+                ControllerUsuario.registodeUsuario()
+            }
+
+            2 -> {
+                ControllerUsuario.iniciodesecion()
+                if (ControllerUsuario.isValidUser("HECTOR", "HCTORkeb12")) {
+                    println("INGRSE UNA OPCION")
+                    println("(1)VER LOS EQUIPPOS DISPONIBLES")
+                    println("(2)REGRISTAR EQUIPO")
+                    val opcionUsuario = readln().toInt()
+                    when (opcionUsuario) {
+                        1-> {
+                            ControllerEquipo.showEquipo()
+                        }
+                        2-> {
+                        ControllerEquipo.regristrodeEquipo()
+                        }
+                    }
+
+                }
+
+
+                }
+            }
+        }
+    }
+
 }
